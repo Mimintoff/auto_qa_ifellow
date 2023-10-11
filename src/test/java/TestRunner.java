d114235c55164e31928c0b4e10a667c2c649f1c7
Михаил  Кочержинский <83241411+Mimintoff@users.noreply.github.com>
2023-10-11 14:37:10 +0300

    added Allure

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,7 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/cucumber.feature", glue = {"StepDefinition", "hooks"}, plugin = {"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"})
+@CucumberOptions(features = "src/test/resources/features/cucumber.feature", glue = {"StepDefinition"}, plugin = {"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"})
 
 public class TestRunner {
 
